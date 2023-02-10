3c512c2d8c88651599a8cea5c90bb44681d64aa5
Amit Das <amit.das@wonderbotz.com>
2023-02-10 19:38:40 +0530

    added user input of name added the c# console project for displaying the input name

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nm;
+            Console.WriteLine("Enter your name: ");
+            nm = Console.ReadLine();
+            Console.WriteLine("your name is : "+nm);
+        }
+    }
+}
